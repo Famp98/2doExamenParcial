9e65be003e2abd350d08649869858ae418eac86e
fabio murguia <fabiomurguiap@gmail.com>
2019-05-25 10:03:25 -0400

    Android como proyecto de inicio y dispositivo creado

---
+++ Parcial2FabioMurguia/Parcial2FabioMurguia/Parcial2FabioMurguia/Views/Notepage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Parcial2FabioMurguia.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Notepage : ContentPage
+	{
+		public Notepage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
